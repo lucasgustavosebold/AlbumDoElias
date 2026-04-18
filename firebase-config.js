d22ded7afcfea8bef916cdc059081d9dd08843f1
Lucas Sebold <lucasgsebold1@gmail.com>
2026-04-17 22:21:15 -0300

    security: mover config Firebase para variáveis de ambiente Netlify

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey:            "AIzaSyBufArWZut2JCo4WWCJmS4yFNa6h218uaI",
-  authDomain:        "album-do-elias.firebaseapp.com",
-  projectId:         "album-do-elias",
-  storageBucket:     "album-do-elias.firebasestorage.app",
-  messagingSenderId: "997429253582",
-  appId:             "1:997429253582:web:e9f7c1ef1b2d985aa2879f"
-};
